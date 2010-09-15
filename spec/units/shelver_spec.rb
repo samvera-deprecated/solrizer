2341ce41a1c60b4cc935676db5b062e18945c98b
rjohns14 <rjohns14@LIB-1224.library.nd.edu>
2010-09-15 12:44:19 -0400

    Add solrize_delete method to delete Fedora object from Blacklight solr

---
+++ spec/units/shelver_spec.rb
@@ -0,0 +1,50 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Solrizer::Solrizer do
+  
+  before(:each) do
+    @solrizer = Solrizer::Solrizer.new
+  end
+  
+  describe "solrize" do
+    it "should trigger the indexer for the provided object" do
+      sample_obj = ActiveFedora::Base.new
+      @solrizer.indexer.expects(:index).with( sample_obj )
+      @solrizer.solrize( sample_obj )
+    end
+    it "should work with Fedora::FedoraObject objects" do
+      mock_object = Fedora::FedoraObject.new(:pid=>"my:pid", :label=>"my label")
+      ActiveFedora::Base.expects(:load_instance).with( mock_object.pid ).returns(mock_object)
+      @solrizer.indexer.expects(:index).with( mock_object )
+      @solrizer.solrize( mock_object )
+    end
+    it "should load the object if only a pid is provided" do
+      mock_object = mock("my object")
+      mock_object.stubs(:pid)
+      mock_object.stubs(:label)
+      mock_object.stubs(:datastreams).returns({'descMetadata'=>"foo","location"=>"bar"})
+
+      ActiveFedora::Base.expects(:load_instance).with( "_PID_" ).returns(mock_object)
+      @solrizer.indexer.expects(:index).with(mock_object)
+      @solrizer.solrize("_PID_")
+    end
+
+  end
+
+  describe "solrize_delete" do
+    it "should call deleteDocument for the indexer" do
+      sample_obj = ActiveFedora::Base.new
+      @solrizer.indexer.expects(:deleteDocument).with(sample_obj.pid)
+      @solrizer.solrize_delete(sample_obj.pid)      
+    end
+  end
+  
+  describe "solrize_objects" do
+    it "should call solrize for each object returned by Fedora::Repository.find_objects" do
+      objects = [["pid1"], ["pid2"], ["pid3"]]
+      Fedora::Repository.any_instance.expects(:find_objects).returns(objects)
+      objects.each {|object| @solrizer.expects(:solrize).with( object ) }
+      @solrizer.solrize_objects
+    end
+  end
+end
