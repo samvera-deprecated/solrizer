6b96c034217309a47b3bd03c1071aa3c84f2277f
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-25 18:12:50 -0500

    moved fedora-specific stuff to solrizer-fedora gem

--- lib/solrizer/fedora.rb
+++
@@ -1,6 +0,0 @@
-require "solrizer"
-module Solrizer::Fedora
-end
-Dir[File.join(File.dirname(__FILE__),"fedora","*.rb")].each {|file| require file }
-
-Solrizer::Extractor.send(:include, Solrizer::Fedora::Extractor)
