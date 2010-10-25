73bd141a3aa05e22497f8f84389b4feac5c05348
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-25 17:47:22 -0500

    minor fix

--- lib/solrizer/html.rb
+++ lib/solrizer/html.rb
@@ -4,4 +4,4 @@ end
 
 Dir[File.join(File.dirname(__FILE__),"html","*.rb")].each {|file| require file }
 
-Solrizer::Extractor.send(:include, Solrizer::XML::Extractor)
+Solrizer::Extractor.send(:include, Solrizer::HTML::Extractor)
