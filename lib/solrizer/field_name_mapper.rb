219e9d3cf90e33e2f720b3a48331035cb56a849d
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-26 17:04:33 -0500

    rearranging class & instance methods for Solrizer::FieldNameMapper module

--- lib/solrizer/field_name_mapper.rb
+++ lib/solrizer/field_name_mapper.rb
@@ -1,34 +1,33 @@
 # Re-Introduced for backwards compatibility
 module Solrizer::FieldNameMapper
-  
-  # Module Methods
-  
-  def self.mappings
-    return {"id"=>"id"}
-  end
-  
-  # @deprecated 
-  # Previously loaded solr field name mappings from a yaml file.  Doesn't do anything any more.
-  def self.load_mappings( arg="arg1")
-    puts "Solrizer::FieldNameMapper.load_mappings doesn't do anything any more.  Stop calling it."
-  end
-  
-  def self.default_field_mapper
-    @@default_field_mapper ||= Solrizer::FieldMapper::Default.new
-  end
-  
-  def self.default_field_mapper=(field_mapper)
-    @@default_field_mapper = field_mapper
-  end
-  
-  def self.solr_name(field_name, field_type, index_type = :searchable)
-    self.default_field_mapper.solr_name(field_name, field_type, index_type)
-  end
-  
+    
   # Class Methods -- These methods will be available on classes that include this Module 
   
   module ClassMethods
+    def mappings
+      return self.default_field_mapper.mappings
+    end
+
+    def id_field
+      return self.default_field_mapper.id_field
+    end
+
+    # Previously loaded solr field name mappings from a yaml file.  Doesn't do anything any more.
+    def load_mappings( config_path=nil)
+      self.default_field_mapper.load_mappings(config_path)
+    end
     
+    def solr_name(field_name, field_type, index_type = :searchable)
+      self.default_field_mapper.solr_name(field_name, field_type, index_type)
+    end
+    
+    def default_field_mapper
+      @@default_field_mapper ||= Solrizer::FieldMapper::Default.new
+    end
+
+    def default_field_mapper=(field_mapper)
+      @@default_field_mapper = field_mapper
+    end
   end
   
   # Instance Methods -- These methods will be available on instances of classes that include this module
@@ -41,7 +40,7 @@ module Solrizer::FieldNameMapper
   
   
   def solr_name(field_name, field_type, index_type = :searchable)   
-    Solrizer::FieldNameMapper.solr_name(field_name, field_type, index_type)
+    self.class.solr_name(field_name, field_type, index_type)
   end
   
 end
--- spec/units/field_name_mapper_spec.rb
+++ spec/units/field_name_mapper_spec.rb
@@ -10,13 +10,13 @@ describe Solrizer::FieldNameMapper do
   
   describe "#mappings" do
     it "should return at least an id_field value" do
-      Solrizer::FieldNameMapper.mappings["id"].should == "id"
+      TestFieldNameMapper.id_field.should == "id"
     end
   end
   
   describe '#solr_name' do
     it "should generate solr field names" do
-      Solrizer::FieldNameMapper.solr_name(:active_fedora_model, :symbol).should == "active_fedora_model_s"
+      TestFieldNameMapper.solr_name(:active_fedora_model, :symbol).should == "active_fedora_model_s"
     end
   end
   
