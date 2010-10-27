fdcd461141c792a59c083749376bcd1a4bc4ae37
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-27 18:23:29 -0500

    require statement fix

--- lib/solrizer.rb
+++ lib/solrizer.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 module Solrizer;end
 
+require "solrizer/extractor"
 # Dir[File.join(File.dirname(__FILE__),"solrizer","*.rb")].each {|file| require file }
 Dir[File.join(File.dirname(__FILE__),"solrizer","*.rb")].each do |file| 
   require "solrizer/"+File.basename(file, File.extname(file))
