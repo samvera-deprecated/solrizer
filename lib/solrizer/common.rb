32989ff968b054c88172d5869e55f61e2d5fe8ff
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-01-14 21:35:51 -0600

    Bump version to 2.1.0

--- lib/solrizer/common.rb
+++ lib/solrizer/common.rb
@@ -1,6 +1,7 @@
 # The goal of this method is to have no dependency on OM, so that NOM or RDF datastreams could use this.
 
 module Solrizer
+  # Instructions on how to solrize the field (types and uses)
   class Directive
     attr_accessor :type, :index_as
     def initialize(*args)
@@ -21,18 +22,12 @@ module Solrizer
     end
 
     module ClassMethods
-
-      def default_field_mapper
-        Solrizer.default_field_mapper
-      end
-
-      def default_field_mapper=(obj)
-        Solrizer.default_field_mapper = obj
-      end
-  
+      # @param [String] field_name_base the name of the solr field (without the type suffix)
+      # @param [Object] value the value to insert into the document
+      # @param [Directive] directive instructions on which fields to create
+      # @param [Hash] solr_doc the solr_doc to insert into.
       def create_and_insert_terms(field_name_base, value, directive, solr_doc)
-        field_mapper ||= default_field_mapper
-        field_mapper.solr_names_and_values(field_name_base, value, directive.type, directive.index_as).each do |field_name, field_value|
+        Solrizer.default_field_mapper.solr_names_and_values(field_name_base, value, directive.type, directive.index_as).each do |field_name, field_value|
           unless field_value.join("").strip.empty?
             ::Solrizer::Extractor.insert_solr_field_value(solr_doc, field_name, field_value)
           end
---
+++ spec/units/common_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Solrizer::Common do
+  before do
+    class Foo
+      include Solrizer::Common
+    end
+  end
+  after do
+     Object.send(:remove_const, :Foo)
+  end
+
+  it "should be awesome" do
+    solr_doc = {}
+    directive = Solrizer::Directive.new(:string, [:displayable, :searchable, :sortable] )
+    Foo.create_and_insert_terms('my_name', 'value', directive, solr_doc)
+    solr_doc.should == {'my_name_t' => ['value'], 'my_name_sort' => ['value'], 'my_name_display' => ['value']}
+  end
+end
