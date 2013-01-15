c933e39aa1dceed60c3d37954b821fc7afe5d6a7
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-01-14 21:06:59 -0600

    No need to require rubygems

--- lib/solrizer.rb
+++ lib/solrizer.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 module Solrizer
   def self.version
     Solrizer::VERSION
@@ -14,7 +13,6 @@ module Solrizer
 end
 
 require "solrizer/extractor"
-# Dir[File.join(File.dirname(__FILE__),"solrizer","*.rb")].each {|file| require file }
 Dir[File.join(File.dirname(__FILE__),"solrizer","*.rb")].each do |file| 
   require "solrizer/"+File.basename(file, File.extname(file)) unless file.match(/railtie.rb$/)
 end
