b88696d2db898223d1aa39d47aa5af832c89eeae
Justin Coyne <justin@curationexperts.com>
2013-05-02 19:45:42 -0500

    use strftime so that a "zulu" date will be returned

--- lib/solrizer/default_descriptors.rb
+++ lib/solrizer/default_descriptors.rb
@@ -114,9 +114,9 @@ module Solrizer
     def self.iso8601_date(value)
       begin 
         if value.is_a?(Date) || value.is_a?(Time)
-          DateTime.parse(value.to_s).to_time.utc.iso8601 
+          DateTime.parse(value.to_s).to_time.utc.strftime('%Y-%m-%dT%H:%M:%SZ') 
         elsif !value.empty?
-          DateTime.parse(value).to_time.utc.iso8601
+          DateTime.parse(value).to_time.utc.strftime('%Y-%m-%dT%H:%M:%SZ')
         end
       rescue ArgumentError => e
         raise ArgumentError, "Unable to parse `#{value}' as a date-time object"
