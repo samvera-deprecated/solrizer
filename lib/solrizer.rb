b1540be4116cf70db79b779530d9f5e4b7e55470
Justin Coyne <digger250@gmail.com>
2013-11-22 16:59:23 -0600

    Update documentation for Solrizer.insert_field [ci skip]

--- lib/solrizer.rb
+++ lib/solrizer.rb
@@ -31,7 +31,7 @@ module Solrizer
 
   # @params [Hash] doc the hash to insert the value into
   # @params [String] name the name of the field (without the suffix)
-  # @params [String,Date] value the value to be inserted
+  # @params [String,Date,Array] value the value (or array of values) to be inserted
   # @params [Array,Hash] indexer_args the arguments that find the indexer
   # @returns [Hash] doc the document that was provided with the new field inserted
   def self.insert_field(doc, name, value, *indexer_args)
