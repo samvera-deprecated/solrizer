50e096d92cbf7c7395c15fde3177c03e63668089
Justin Coyne <justin@curationexperts.com>
2014-07-17 10:26:11 -0500

    Fix rspec deprecations

--- spec/units/solrizer_spec.rb
+++ spec/units/solrizer_spec.rb
@@ -7,68 +7,68 @@ describe Solrizer do
       let(:doc) { Hash.new }
       it "should insert a field with the default (stored_searchable) indexer" do
         Solrizer.insert_field(doc, 'foo', 'A name')
-        doc.should == {'foo_tesim' => ['A name']}
+        expect(doc).to eq('foo_tesim' => ['A name'])
       end
       it "should not create an array of fields that are not multivalued" do
         Solrizer.insert_field(doc, 'foo', 'A name', :sortable)
-        doc.should == {'foo_si' => 'A name'}
+        expect(doc).to eq('foo_si' => 'A name')
       end
       it "should insert a field with multiple indexers" do
         Solrizer.insert_field(doc, 'foo', 'A name', :sortable, :facetable)
-        doc.should == {'foo_si' => 'A name', 'foo_sim' => ['A name']}
+        expect(doc).to eq('foo_si' => 'A name', 'foo_sim' => ['A name'])
       end
       it "should insert Dates" do
         Solrizer.insert_field(doc, 'foo', Date.parse('2013-01-13'))
-        doc.should == {'foo_dtsim' => ["2013-01-13T00:00:00Z"]}
+        expect(doc).to eq('foo_dtsim' => ["2013-01-13T00:00:00Z"])
       end
       it "should insert Times" do
         Solrizer.insert_field(doc, 'foo', Time.parse('2013-01-13T22:45:56+06:00'))
-        doc.should == {'foo_dtsim' => ["2013-01-13T16:45:56Z"]}
+        expect(doc).to eq('foo_dtsim' => ["2013-01-13T16:45:56Z"])
       end
       it "should insert true Booleans" do
         Solrizer.insert_field(doc, 'foo', true)
-        doc.should == {'foo_bsi' => true}
+        expect(doc).to eq('foo_bsi' => true)
       end
       it "should insert false Booleans" do
         Solrizer.insert_field(doc, 'foo', false)
-        doc.should == {'foo_bsi' => false}
+        expect(doc).to eq('foo_bsi' => false)
       end
 
       it "should insert multiple values" do
         Solrizer.insert_field(doc, 'foo', ['A name', 'B name'], :sortable, :facetable)
-        doc.should == {'foo_si' => 'B name', 'foo_sim' => ['A name', 'B name']}
+        expect(doc).to eq('foo_si' => 'B name', 'foo_sim' => ['A name', 'B name'])
       end
 
       it 'should insert nothing when passed a nil value' do
         Solrizer.insert_field(doc, 'foo', nil, :sortable, :facetable)
-        doc.should == {}
+        expect(doc).to eq( { } )
       end
     end
 
     describe "on a document with values" do
-      before{ @doc = {'foo_si' => 'A name', 'foo_sim' => ['A name']}}
+      let(:doc) { {'foo_si' => 'A name', 'foo_sim' => ['A name']} }
 
       it "should not overwrite muli-values that exist before" do
-        Solrizer.insert_field(@doc, 'foo', 'B name', :sortable, :facetable)
-        @doc.should == {'foo_si' => 'B name', 'foo_sim' => ['A name', 'B name']}
+        Solrizer.insert_field(doc, 'foo', 'B name', :sortable, :facetable)
+        expect(doc).to eq('foo_si' => 'B name', 'foo_sim' => ['A name', 'B name'])
       end
     end
   end
   describe ".set_field" do
     describe "on a document with values" do
-      before{ @doc = {'foo_si' => ['A name'], 'foo_sim' => ['A name']}}
+      let(:doc) { {'foo_si' => ['A name'], 'foo_sim' => ['A name']} }
 
       it "should overwrite values that exist before" do
-        Solrizer.set_field(@doc, 'foo', 'B name', :sortable, :facetable)
-        @doc.should == {'foo_si' => 'B name', 'foo_sim' => ['B name']}
+        Solrizer.set_field(doc, 'foo', 'B name', :sortable, :facetable)
+        expect(doc).to eq('foo_si' => 'B name', 'foo_sim' => ['B name'])
       end
     end
   end
 
   describe ".solr_name" do
     it "should delegate to default_field_mapper" do
-        Solrizer.solr_name('foo', type: :string).should == "foo_tesim"
-        Solrizer.solr_name('foo', :sortable).should == "foo_si"
+        expect(Solrizer.solr_name('foo', type: :string)).to eq "foo_tesim"
+        expect(Solrizer.solr_name('foo', :sortable)).to eq "foo_si"
     end
   end
 end
--- spec/units/xml_extractor_spec.rb
+++ spec/units/xml_extractor_spec.rb
@@ -10,16 +10,16 @@ describe Solrizer::XML::Extractor do
   
   describe ".xml_to_solr" do
     it "should turn simple xml into a solr document" do
-      result[:type_tesim].should == "text"
-      result[:medium_tesim].should == "Paper Document"
-      result[:rights_tesim].should == "Presumed under copyright. Do not publish."
-      result[:date_tesim].should == "1985-12-30"
-      result[:format_tesim].should be_kind_of(Array)
-      result[:format_tesim].should include("application/tiff")
-      result[:format_tesim].should include("application/pdf")
-      result[:format_tesim].should include("application/jp2000")
-      result[:title_tesim].should == "This is a Sample Title"
-      result[:publisher_tesim].should == "Sample Unversity"
+      expect(result[:type_tesim]).to eq "text"
+      expect(result[:medium_tesim]).to eq "Paper Document"
+      expect(result[:rights_tesim]).to eq "Presumed under copyright. Do not publish."
+      expect(result[:date_tesim]).to eq "1985-12-30"
+      expect(result[:format_tesim]).to be_kind_of(Array)
+      expect(result[:format_tesim]).to include("application/tiff")
+      expect(result[:format_tesim]).to include("application/pdf")
+      expect(result[:format_tesim]).to include("application/jp2000")
+      expect(result[:title_tesim]).to eq "This is a Sample Title"
+      expect(result[:publisher_tesim]).to eq "Sample Unversity"
     end
   end
   
